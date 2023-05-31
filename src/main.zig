b56f5f44b564cdf4f5661adeaaaae02d38399c9f
Dominik <dominik.kurasbediani@gmail.com>
2023-05-31 15:03:37 +0200

    Properly parses assembled files

--- src/main.zig
+++ src/main.zig
@@ -10,14 +10,16 @@ pub fn main() !void {
     }
     var argIter = try std.process.argsWithAllocator(alloc);
     defer argIter.deinit();
-    while(argIter.next()) |iter| {
-        debug.print("{s}\n", .{iter});
-        const file = try std.fs.cwd().createFile(iter, .{.read = true});
-        try file.seekTo(0);
-        const contents = try file.reader().readAllAlloc(alloc, 128);
-        debug.print("{s}\n", .{contents});
-        // const stat = try file.stat();
-        // debug.print("File size: {d}\n", .{stat.size});
-        defer file.close();
-    }
+    // while(argIter.next()) |iter| {
+    //     debug.print("{s}\n", .{iter});
+    // }
+    var path_buffer: [std.fs.MAX_PATH_BYTES]u8 = undefined;
+    const path = try std.fs.cwd().realpath("mov", &path_buffer);
+    debug.print("{s}\n", .{path});
+    const file = try std.fs.openFileAbsolute(path, .{});
+    defer file.close();
+    try file.seekTo(0);
+    const contents = try file.reader().readAllAlloc(alloc, std.fs.MAX_PATH_BYTES);
+    defer alloc.free(contents);
+    debug.print("{b}\n", .{contents});
 }
